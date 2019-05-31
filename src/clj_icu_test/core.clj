1ac66ac99dc873377e9816c34871b482dd114d2b
Elango Cheran <elango.cheran@gmail.com>
2019-05-31 16:54:34 -0700

    adds emitting cpp def

--- src/clj_icu_test/core.clj
+++ src/clj_icu_test/core.clj
@@ -2,6 +2,76 @@
   (:require [clojure.string :as string]
             [clojure.tools.analyzer.jvm :as az]))
 
+;;
+;; C++
+;;
+
+(def emit-cpp)
+
+(defn emit-cpp-type
+  [class]
+  (when class
+    (let [canonical-name (.getCanonicalName class)
+          java-cpp-type-map {java.lang.Integer "int"
+                             java.lang.Long "long int"
+                             java.lang.Float "float"
+                             java.lang.Double "double float"
+                             java.lang.Boolean "bool"
+                             java.lang.String "string"}]
+      (if-let [transformed-type (get java-cpp-type-map class)]
+        transformed-type
+        canonical-name))))
+
+(defn emit-cpp-statement
+  [statement-parts]
+  (str (->> statement-parts
+            (keep identity)
+            (map str)
+            (string/join " "))
+       ";"))
+
+(defn emit-cpp-const
+  [ast]
+  {:pre [(= :const (:op ast))
+         (:literal? ast)]}
+  (str (:val ast)))
+
+;; bindings
+
+(defn emit-cpp-def
+  [ast]
+  {:pre [(= :def (:op ast))]}
+  (let [type-class (or (get-in ast [:meta :val :tag])
+                       (get-in ast [:init :env :tag]))
+        type-str (emit-cpp-type type-class)
+        identifier (name (:name ast))
+        expression (emit-cpp (:init ast))
+        statement-parts [type-str
+                         identifier
+                         "="
+                         expression]
+        statement (emit-cpp-statement statement-parts)]
+    statement))
+
+;; entry point
+
+(defn emit-cpp
+  [ast]
+  ;; TODO: some multimethod ?
+  (case (:op ast)
+    :def (emit-cpp-def ast)
+    :const (emit-cpp-const ast))
+  )
+
+
+;;
+;; Java
+;;
+
+;; common forms
+
+(def emit-java)
+
 (defn emit-java-type
   [class]
   (when class
@@ -24,6 +94,8 @@
          (:literal? ast)]}
   (str (:val ast)))
 
+;; bindings
+
 (defn emit-java-def
   [ast]
   {:pre [(= :def (:op ast))]}
@@ -38,6 +110,8 @@
                          expression]
         statement (emit-java-statement statement-parts)]
     statement))
+
+;; entry point
 
 (defn emit-java
   [ast]
--- test/clj_icu_test/core_test.clj
+++ test/clj_icu_test/core_test.clj
@@ -3,9 +3,16 @@
             [clojure.tools.analyzer.jvm :as az]
             [clj-icu-test.core :refer :all]))
 
-(deftest a-test
+(deftest java-binding-test
   (testing "java - def"
     (let [ast (az/analyze '(def x 3))] 
       (is (= "x = 3;" (emit-java ast))))
-    (let [ast (az/analyze '(def ^Integer x 3))]
-      (is (= "Integer x = 3;" (emit-java ast))))))
+    (let [ast (az/analyze '(def ^Integer x 5))]
+      (is (= "Integer x = 5;" (emit-java ast))))))
+
+(deftest cpp-binding-test
+  (testing "cpp - def"
+    (let [ast (az/analyze '(def x 3))]
+      (is (= "x = 3;" (emit-cpp ast))))
+    (let [ast (az/analyze '(def ^Integer x 5))]
+      (is (= "int x = 5;" (emit-cpp ast))))))
