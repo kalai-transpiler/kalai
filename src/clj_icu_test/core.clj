4806804605023ecc2c316c040b7a42873e4d79c0
Elango Cheran <elango.cheran@gmail.com>
2019-06-06 22:59:49 -0700

    adds do (multiple expressions) and atom/reset! (re-/bindings needed for let forms)

--- src/clj_icu_test/core.clj
+++ src/clj_icu_test/core.clj
@@ -13,10 +13,13 @@
   (when class
     (let [canonical-name (.getCanonicalName class)
           java-cpp-type-map {java.lang.Integer "int"
+                             int "int"
                              java.lang.Long "long int"
+                             long "long int"
                              java.lang.Float "float"
                              java.lang.Double "double float"
                              java.lang.Boolean "bool"
+                             boolean "bool"
                              java.lang.String "string"}]
       (if-let [transformed-type (get java-cpp-type-map class)]
         transformed-type
@@ -75,10 +78,15 @@
 (defn emit-java-type
   [class]
   (when class
-    (let [canonical-name (.getCanonicalName class)]
-      (if (.startsWith canonical-name "java.lang.")
-        (subs canonical-name 10)
-        canonical-name))))
+    (cond
+      (= long class) "long"
+      (= int class) "int"
+      (= char class) "char"
+      (= boolean class) "boolean"
+      :else (let [canonical-name (.getCanonicalName class)]
+              (if (.startsWith canonical-name "java.lang.")
+                (subs canonical-name 10)
+                canonical-name)))))
 
 (defn emit-java-statement
   [statement-parts]
@@ -94,7 +102,41 @@
          (:literal? ast)]}
   (str (:val ast)))
 
+(defn emit-java-do
+  [ast]
+  {:pre [(= :do (:op ast))]}
+  (let [stmts (:statements ast)
+        stmt-emitted-lines (map emit-java stmts)
+        last-stmt (:ret ast)
+        last-emitted-line (emit-java last-stmt)
+        all-lines (concat stmt-emitted-lines [last-emitted-line])]
+    all-lines))
+
 ;; bindings
+
+(defn emit-java-atom
+  [ast]
+  {:pre [(and (= :invoke (:op ast))
+              (= (symbol "atom") (-> ast :fn :meta :name)))]}
+  (let [init-val-ast (-> ast
+                         :args
+                         first)]
+    (emit-java init-val-ast)))
+
+(defn emit-java-reset!
+  [ast]
+  {:pre [(and (= :invoke (:op ast))
+              (= (symbol "reset!") (-> ast :fn :meta :name)))]}
+  (let [identifier (-> ast :args first :meta :name name)
+        reset-val-ast (-> ast
+                          :args
+                          second)
+        expression (-> reset-val-ast emit-java)
+        statement-parts [identifier
+                         "="
+                         expression]
+        statement (emit-java-statement statement-parts)]
+    statement))
 
 (defn emit-java-def
   [ast]
@@ -118,5 +160,8 @@
   ;; TODO: some multimethod ?
   (case (:op ast)
     :def (emit-java-def ast)
-    :const (emit-java-const ast))
-  )
+    :const (emit-java-const ast)
+    :invoke (case (-> ast :fn :meta :name name)
+              "atom" (emit-java-atom ast)
+              "reset!" (emit-java-reset! ast))
+    :do (emit-java-do ast)))
--- test/clj_icu_test/core_test.clj
+++ test/clj_icu_test/core_test.clj
@@ -10,6 +10,27 @@
     (let [ast (az/analyze '(def ^Integer x 5))]
       (is (= "Integer x = 5;" (emit-java ast))))))
 
+(deftest java-multiple-expressions
+  (testing "java - do"
+    (let [ast (az/analyze '(do (def x 3) (def y 5)))]
+      (is (= (emit-java ast) ["x = 3;"
+                              "y = 5;"])))
+    (let [ast (az/analyze '(do (def ^Boolean x true) (def ^Long y 5)))]
+      (is (= (emit-java ast) ["Boolean x = true;"
+                              "Long y = 5;"])))))
+
+(deftest java-binding-test-atoms
+  (testing "java - atom"
+    (let [ast (az/analyze '(def x (atom 11)))]
+      (is (= (emit-java ast) "x = 11;"))))
+  (testing "java - reset!"
+    (let [ast (az/analyze '(do (def x (atom 11)) (reset! x 13)))]
+      (is (= (emit-java ast) ["x = 11;"
+                              "x = 13;"])))
+    (let [ast (az/analyze '(do (def ^Long x (atom 11)) (reset! x 13)))]
+      (is (= (emit-java ast) ["Long x = 11;"
+                              "x = 13;"])))))
+
 (deftest cpp-binding-test
   (testing "cpp - def"
     (let [ast (az/analyze '(def x 3))]
