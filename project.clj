3ff6177979a0a8e26724a14e1d555b2c7c0c7c3e
Elango Cheran <elango.cheran@gmail.com>
2019-06-27 23:23:07 -0700

    switches testing library from clojure.test to expectations

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.analyzer.jvm "0.7.2"]]
+  :profiles {:dev {:dependencies [[expectations "2.1.10"]]}}
   :repl-options {:init-ns clj-icu-test.core})
--- test/clj_icu_test/core_test.clj
+++ test/clj_icu_test/core_test.clj
@@ -1,102 +1,131 @@
 (ns clj-icu-test.core-test
-  (:require [clojure.test :refer :all]
+  (:require ;;[clojure.test :refer :all]
             [clojure.tools.analyzer.jvm :as az]
-            [clj-icu-test.core :refer :all]))
+            [clj-icu-test.core :refer :all]
+            [expectations :refer :all]))
 ;;
 ;; C++
 ;;
 
-(deftest cpp-binding-test
-  (testing "cpp - def"
-    (let [ast (az/analyze '(def x 3))]
-      (is (= "x = 3;" (emit-cpp ast))))
-    (let [ast (az/analyze '(def ^Integer x 5))]
-      (is (= "int x = 5;" (emit-cpp ast))))))
+;; bindings
+
+;; bindings - def
+
+(let [ast (az/analyze '(def x 3))]
+  (expect "x = 3;" (emit-cpp ast)))
+
+(let [ast (az/analyze '(def ^Integer x 5))]
+  (expect "int x = 5;" (emit-cpp ast)))
 
 ;;
 ;; Java
 ;;
 
-(deftest java-binding-test
-  (testing "java - def"
-    (let [ast (az/analyze '(def x 3))] 
-      (is (= "x = 3;" (emit-java ast))))
-    (let [ast (az/analyze '(def ^Integer x 5))]
-      (is (= "Integer x = 5;" (emit-java ast))))))
+;; bindings
 
-(deftest java-multiple-expressions
-  (testing "java - do"
-    (let [ast (az/analyze '(do (def x 3) (def y 5)))]
-      (is (= (emit-java ast) ["x = 3;"
-                              "y = 5;"])))
-    (let [ast (az/analyze '(do (def ^Boolean x true) (def ^Long y 5)))]
-      (is (= (emit-java ast) ["Boolean x = true;"
-                              "Long y = 5;"])))))
+;; bindings - def
 
-(deftest java-binding-test-atoms
-  (testing "java - atom"
-    (let [ast (az/analyze '(def x (atom 11)))]
-      (is (= (emit-java ast) "x = 11;"))))
-  (testing "java - reset!"
-    (let [ast (az/analyze '(do (def x (atom 11)) (reset! x 13)))]
-      (is (= (emit-java ast) ["x = 11;"
-                              "x = 13;"])))
-    (let [ast (az/analyze '(do (def ^Long x (atom 11)) (reset! x 13)))]
-      (is (= (emit-java ast) ["Long x = 11;"
-                              "x = 13;"])))))
+(let [ast (az/analyze '(def x 3))] 
+  (expect "x = 3;" (emit-java ast)))
 
-(deftest java-static-call-test
-  (testing "java - arithmetic fn - +"
-    (let [ast (az/analyze '(+ 11 17))]
-      (is (= (emit-java ast) "11 + 17"))))
-  (testing "java - arithmetic fn - /"
-    (let [ast (az/analyze '(/ 34 17))]
-      (is (= (emit-java ast) "34 / 17"))))
-  (testing "java - arithmetic fn - multiple operands"
-    (let [ast (az/analyze '(+ 11 17 19 23))]
-      (is (= (emit-java ast) "11 + 17 + 19 + 23")))))
+(let [ast (az/analyze '(def ^Integer x 5))]
+  (expect "Integer x = 5;" (emit-java ast)))
 
-(deftest java-binding-test-let
-  (testing "java - let - 1 expression"
-    (let [ast (az/analyze '(let [x 1] (+ x 3)))]
-      (is (= (emit-java ast)
-"{
+;; language - multiple expressions
+
+;; language - multiple expressions - do block
+
+(let [ast (az/analyze '(do (def x 3) (def y 5)))]
+  (expect (emit-java ast) ["x = 3;"
+                      "y = 5;"]))
+
+(let [ast (az/analyze '(do (def ^Boolean x true) (def ^Long y 5)))]
+  (expect (emit-java ast) ["Boolean x = true;"
+                           "Long y = 5;"]))
+
+;; bindings
+
+;; bindings - atoms
+
+(let [ast (az/analyze '(def x (atom 11)))]
+  (expect (emit-java ast) "x = 11;"))
+
+;; bindings - reset!
+
+(let [ast (az/analyze '(do (def x (atom 11)) (reset! x 13)))]
+  (expect (emit-java ast) ["x = 11;"
+                      "x = 13;"]))
+
+(let [ast (az/analyze '(do (def ^Long x (atom 11)) (reset! x 13)))]
+  (expect (emit-java ast) ["Long x = 11;"
+                           "x = 13;"]))
+
+;; static call (arithmetic operations)
+
+;; static call - +
+
+(let [ast (az/analyze '(+ 11 17))]
+  (expect (emit-java ast) "11 + 17"))
+
+;; static call - /
+
+(let [ast (az/analyze '(/ 34 17))]
+  (expect (emit-java ast) "34 / 17"))
+
+;; language - multiple operands
+
+(let [ast (az/analyze '(+ 11 17 19 23))]
+  (expect (emit-java ast) "11 + 17 + 19 + 23"))
+
+;; bindings - let
+
+;; bindings - let - 1 expression
+
+(let [ast (az/analyze '(let [x 1] (+ x 3)))]
+  (expect (emit-java ast)
+     "{
   x = 1;
   x + 3;
-}"))))
-  (testing "java - let - 2 expressions"
-    (let [ast (az/analyze '(let [x 1] (+ x 3) (+ x 5)))]
-      (is (= (emit-java ast)
-"{
+}"))
+
+;; bindings - let - 2 expressions
+
+(let [ast (az/analyze '(let [x 1] (+ x 3) (+ x 5)))]
+  (expect (emit-java ast)
+     "{
   x = 1;
   x + 3;
   x + 5;
-}"))))
-  (testing "java - let - 2 bindings"
-    (let [ast (az/analyze '(let [x 1 y 2] (* x y)))]
-      (is (= (emit-java ast)
-"{
+}"))
+
+;; bindings - let - 2 bindings
+
+(let [ast (az/analyze '(let [x 1 y 2] (* x y)))]
+  (expect (emit-java ast)
+     "{
   x = 1;
   y = 2;
   x * y;
-}"))))
-  (testing "java - let - 2 bindings - expression in binding"
-    (let [ast (az/analyze '(let [x 5 y (* x x)] (+ x y)))]
-      (is (= (emit-java ast)
-"{
+}"))
+
+;; bindings - let - 2 bindings - expression in binding
+
+(let [ast (az/analyze '(let [x 5 y (* x x)] (+ x y)))]
+  (expect (emit-java ast)
+     "{
   x = 5;
   y = x * x;
   x + y;
-}")))))
+}"))
 
-(deftest java-let-test-nesting
-  (testing "nested let"
-    (let [ast (az/analyze '(let [x 5] (let [y (* x x)] (/ y x))))]
-      (is (= (emit-java ast)
-             "{
+;; bindings - let - nesting of let forms
+
+(let [ast (az/analyze '(let [x 5] (let [y (* x x)] (/ y x))))]
+  (expect (emit-java ast)
+     "{
   x = 5;
   {
     y = x * x;
     y / x;
   }
-}")))))
+}"))
