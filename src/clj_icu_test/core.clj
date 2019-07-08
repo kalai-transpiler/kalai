fe4057a49763c3107f55f548ffbf7c53faeb9bdc
Elango Cheran <elango.cheran@gmail.com>
2019-07-08 00:28:53 -0700

    genericizes AST arg to emitter fns as a record that includes environment and target language;  uses env field of record to enable analyzing of raw forms of sub-expressions, which enables proper emission of expressions with nested args

--- src/clj_icu_test/core.clj
+++ src/clj_icu_test/core.clj
@@ -10,6 +10,8 @@
 
 (def INDENT-LEVEL-INCREMENT 2)
 
+(def LANGS {:java :cpp})
+
 ;;
 ;; indentation - global state and fns
 ;;
@@ -33,6 +35,19 @@
 (defn indent-str-curr-level
   []
   (apply str (repeat @indent-level INDENT-CHAR)))
+
+;;
+;; records
+;;
+
+(defrecord AstOpts [ast env lang])
+
+(defn new-ast-opts
+  "keys of the opts map are the fields in AstOpts record"
+  [opts]
+  {:pre [(or (not (:lang opts))
+             (get LANGS (:lang opts)))]}
+  (map->AstOpts opts))
 
 ;;
 ;; C++
@@ -66,10 +81,11 @@
        ";"))
 
 (defn emit-cpp-const
-  [ast]
-  {:pre [(= :const (:op ast))
-         (:literal? ast)]}
-  (str (:val ast)))
+  [ast-opts]
+  {:pre [(= :const (:op (:ast ast-opts)))
+         (:literal? (:ast ast-opts))]}  
+  (let [ast (:ast ast-opts)]
+    (str (:val ast))))
 
 ;; bindings
 
@@ -77,8 +93,9 @@
 (defn emit-cpp-assignment
   "To be used by both 'def' and any bindings block of a form (ex: let)
   Might return nil"
-  [ast]
-  (let [op-code (:op ast)
+  [ast-opts]
+  (let [ast (:ast ast-opts)
+        op-code (:op ast)
         type-class (or (get-in ast [:meta :val :tag])
                        (get-in ast [:init :env :tag]))
         type-str (emit-cpp-type type-class)
@@ -87,7 +104,7 @@
                                                      :binding (get ast :form)
                                                      :def (get ast :name)))]
                      (str identifer-symbol))
-        expression (emit-cpp (:init ast))
+        expression (emit-cpp (assoc ast-opts :ast (:init ast)))
         statement-parts [type-str
                          identifier
                          "="
@@ -96,18 +113,20 @@
     statement))
 
 (defn emit-cpp-def
-  [ast]
-  {:pre [(= :def (:op ast))]}
-  (emit-cpp-assignment ast))
+  [ast-opts]
+  {:pre [(= :def (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)]
+    (emit-cpp-assignment ast-opts)))
 
 ;; entry point
 
 (defn emit-cpp
-  [ast]
+  [ast-opts]
   ;; TODO: some multimethod ?
-  (case (:op ast)
-    :def (emit-cpp-def ast)
-    :const (emit-cpp-const ast))
+  (let [ast (:ast ast-opts)]
+    (case (:op ast)
+      :def (emit-cpp-def ast-opts)
+      :const (emit-cpp-const ast-opts)))
   )
 
 
@@ -121,7 +140,7 @@
 
 (defn emit-java-type
   "Might return nil"
-  [class]
+  [class]  
   (when class
     (cond
       ;; TODO: uncomment the primitive type class code unless and until we want to have
@@ -131,7 +150,7 @@
       ;; (= char class) "char"
       ;; (= boolean class) "boolean"
       :else (let [canonical-name (.getCanonicalName class)] 
-              (cond
+              (cond                
                 ;; this is to prevent the analyzer from auto-tagging the type classes of
                 ;; symbols in a binding form in a way that is currently being assumed to
                 ;; be unwanted in the emitted output.
@@ -175,42 +194,47 @@
     result))
 
 (defn emit-java-const
-  [ast]
-  {:pre [(= :const (:op ast))
-         (:literal? ast)]}
-  (str (:val ast)))
+  [ast-opts]
+  {:pre [(= :const (:op (:ast ast-opts)))
+         (:literal? (:ast ast-opts))]}
+  (let [ast (:ast ast-opts)]
+    (str (:val ast))))
 
 (defn emit-java-do
-  [ast]
-  {:pre [(= :do (:op ast))]}
-  (let [stmts (:statements ast)
-        stmt-emitted-lines (map emit-java stmts)
+  [ast-opts]
+  {:pre [(= :do (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)
+        stmts (:statements ast)
+        stmts-ast-opts (map (partial assoc ast-opts :ast) stmts)
+        stmt-emitted-lines (map emit-java stmts-ast-opts)
         last-stmt (:ret ast)
-        last-emitted-line (emit-java last-stmt)
+        last-emitted-line (emit-java (assoc ast-opts :ast last-stmt))
         all-lines (concat stmt-emitted-lines [last-emitted-line])]
     all-lines))
 
 ;; bindings
 
 (defn emit-java-atom
-  [ast]
-  {:pre [(and (= :invoke (:op ast))
-              (= (symbol "atom") (-> ast :fn :meta :name)))]}
-  (let [init-val-ast (-> ast
+  [ast-opts]
+  {:pre [(and (= :invoke (:op (:ast ast-opts)))
+              (= (symbol "atom") (-> ast-opts :ast :fn :meta :name)))]}
+  (let [ast (:ast ast-opts)
+        init-val-ast (-> ast
                          :args
                          first)]
-    (emit-java init-val-ast)))
+    (emit-java (assoc ast-opts :ast init-val-ast))))
 
 (defn emit-java-reset!
-  [ast]
-  {:pre [(and (= :invoke (:op ast))
-              (= (symbol "reset!") (-> ast :fn :meta :name)))]}
-  (let [identifier (str (or (-> ast :args first :meta :name)
+  [ast-opts]
+  {:pre [(and (= :invoke (:op (:ast ast-opts)))
+              (= (symbol "reset!") (-> ast-opts :ast :fn :meta :name)))]}
+  (let [ast (:ast ast-opts)
+        identifier (str (or (-> ast :args first :meta :name)
                             (-> ast :args first :form)))
         reset-val-ast (-> ast
                           :args
                           second)
-        expression (-> reset-val-ast emit-java)
+        expression (emit-java (assoc ast-opts :ast reset-val-ast))
         statement-parts [identifier
                          "="
                          expression]
@@ -220,44 +244,49 @@
 (defn emit-java-assignment
   "To be used by both 'def' and any bindings block of a form (ex: let)
   Might return nil"
-  [ast]
-  (let [op-code (:op ast)
+  [ast-opts]
+  (let [ast (:ast ast-opts)
+        op-code (:op ast) 
         type-class (or (get-in ast [:meta :val :tag])
                        (get-in ast [:init :env :tag])
                        (and (= :binding op-code)
-                            (get ast :tag)))
+                            (get ast :tag)))        
         type-str (emit-java-type type-class)
         identifier (when-let [identifer-symbol (or (get-in ast [:env :form])
                                                    (case op-code
                                                      :binding (get ast :form)
                                                      :def (get ast :name)))]
                      (str identifer-symbol))
-        expression (emit-java (:init ast))
+        expression (emit-java (assoc ast-opts :ast (:init ast)))
         statement-parts [type-str
                          identifier
                          "="
-                         expression]
+                         expression]        
         statement (emit-java-statement statement-parts)]
     statement))
 
 (defn emit-java-def
-  [ast]
-  {:pre [(= :def (:op ast))]}
-  (emit-java-assignment ast))
+  [ast-opts]
+  {:pre [(= :def (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)]
+    (emit-java-assignment ast-opts)))
 
 (defn emit-java-binding
   "Might return nil"
-  [ast]
-  {:pre [(= :binding (:op ast))]}
-  (emit-java-assignment ast))
+  [ast-opts]
+  {:pre [(= :binding (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)]
+    (emit-java-assignment ast-opts)))
 
 (defn emit-java-bindings-stanza
   "The bindings stanza in a form (ex: in a let form).
   Might return nil"
-  [ast]
-  {:pre [(sequential? ast)]}
-  (let [bindings ast ;; the AST in this case is a vector of sub-ASTs
-        binding-statements (map emit-java-binding bindings)]
+  [ast-opts]
+  {:pre [(sequential? (:ast ast-opts))]}
+  (let [ast (:ast ast-opts)
+        bindings ast ;; the AST in this case is a vector of sub-ASTs
+        bindings-ast-opts (map (partial assoc ast-opts :ast) bindings)
+        binding-statements (map emit-java-binding bindings-ast-opts)]
     (when-let [non-nil-binding-statements (->> binding-statements
                                                (keep identity)
                                                seq)]
@@ -265,10 +294,11 @@
         bindings-str))))
 
 (defn emit-java-let
-  [ast]
-  {:pre [(= :let (:op ast))]}
-  (let [bindings (:bindings ast)
-        binding-str (indent (emit-java-bindings-stanza bindings))
+  [ast-opts]
+  {:pre [(= :let (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)
+        bindings (:bindings ast)
+        binding-str (indent (emit-java-bindings-stanza (assoc ast-opts :ast bindings)))
         body-ast (:body ast)
         body-strs (indent
                    (if (:statements body-ast)
@@ -276,12 +306,16 @@
                      (let [butlast-statements (:statements body-ast)
                            last-statement (:ret body-ast)
                            statements (concat butlast-statements [last-statement])
-                           statement-strs (map emit-java statements)]
+                           statement-ast-opts (map #(assoc ast-opts :ast %) statements)
+                           statement-strs (map emit-java statement-ast-opts)]
                        statement-strs)
                      ;; else the let block has only one "statement" in the do block
-                     [(emit-java (:body ast))]))
+                     [(emit-java (assoc ast-opts :ast (:body ast)))]))
         body-strs-with-semicolons (indent
-                                   (map #(if-not (can-become-java-statement %) % (emit-java-statement [%])) body-strs))
+                                   (map #(if-not (can-become-java-statement %)
+                                           %
+                                           (emit-java-statement [%]))
+                                        body-strs))
         body-str (string/join "\n" body-strs-with-semicolons)
         block-str-parts [(str (indent-str-curr-level) "{")
                          binding-str
@@ -294,30 +328,67 @@
 
 ;; "arithmetic" (built-in operators)
 
-(defn flatten-static-call-args
-  "Convert the :args field of the ast into a seq of subtrees of operands, but also flatten any nesting of :args-keyed operands into a flat seq.
-  Recursive function.
-  May return nil"
-  [ast]
-  (when ast
-    (if-not (sequential? (:args ast))
-      [(:args ast)]
-      (->> (mapcat
-            (fn [arg-ast] (if (:args arg-ast)
-                            (flatten-static-call-args arg-ast)
-                            [arg-ast]))
-             (:args ast))
-           (keep identity)))))
+;; (defn flatten-static-call-args
+;;   "Convert the :args field of the ast into a seq of subtrees of operands, but also flatten any nesting of :args-keyed operands into a flat seq.
+;;   Recursive function.
+;;   May return nil"
+;;   [ast]
+;;   (when ast
+;;     (if-not (sequential? (:args ast))
+;;       [(:args ast)]
+;;       (->> (mapcat
+;;             (fn [arg-ast] (if (:args arg-ast)
+;;                             (flatten-static-call-args arg-ast)
+;;                             [arg-ast]))
+;;              (:args ast))
+;;            (keep identity)))))
+
+(defn emit-java-arg
+  [ast-opts symb]
+  {:pre [(:env ast-opts)]}
+  (let [ast-opts-env (:env ast-opts)
+        symb-class (class symb)
+        symb-ast (az/analyze symb ast-opts-env)
+        symb-ast-opts {:ast symb-ast}]
+    (cond
+
+      ;; emit a "standalone" token
+      (= clojure.lang.Symbol symb-class)
+      (emit-java symb-ast-opts)
+      
+      ;; a seq of symbols -> emit parenthases around the emitted form
+      (or (isa? symb-class clojure.lang.IPersistentCollection)
+          (isa? symb-class clojure.lang.ISeq))
+      (if (= 1 (count symb-ast))
+        (emit-java (assoc symb-ast-opts :ast (first symb-ast)))
+        (str "(" (emit-java symb-ast-opts) ")"))
+
+      ;; else, we have something that we treat like a scalar
+      :else (emit-java symb-ast-opts))))
+
+(defn emit-java-args
+  [ast-opts]
+  {:pre [(-> ast-opts :ast :raw-forms seq)]}
+  (let [ast (:ast ast-opts)
+        raw-forms (-> ast :raw-forms)
+        raw-form-arg-symbols (-> raw-forms
+                                 last
+                                 rest)
+        raw-form-arg-symbol-ast-opts {:env (-> ast :env)}
+        emitted-args (map (partial emit-java-arg raw-form-arg-symbol-ast-opts) raw-form-arg-symbols)]
+    emitted-args))
 
 (defn emit-java-static-call
-  [ast]
-  {:pre [(= :static-call (:op ast))]}
-  (let [static-call-fn-symbol (-> ast :raw-forms last first)
-        flattened-args (or (when (-> ast :body :args)
-                             (flatten-static-call-args (-> ast :body)))
-                           (when (-> ast :args)
-                             (flatten-static-call-args ast)))
-        arg-strs (map emit-java flattened-args)
+  [ast-opts]
+  {:pre [(= :static-call (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)
+        static-call-fn-symbol (-> ast :raw-forms last first)
+        ;; flattened-args (or (when (-> ast :body :args)
+        ;;                      (flatten-static-call-args (-> ast :body)))
+        ;;                    (when (-> ast :args)
+        ;;                      (flatten-static-call-args ast)))
+        ;; arg-strs (map emit-java flattened-args)
+        arg-strs (emit-java-args ast-opts)
         expression-parts (interpose static-call-fn-symbol arg-strs)
         expression (string/join " " expression-parts)]
     expression))
@@ -326,34 +397,36 @@
 ;; other
 
 (defn emit-java-local
-  [ast]
-  {:pre [(= :local (:op ast))]}
-  (let [form (:form ast)]
+  [ast-opts]
+  {:pre [(= :local (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)
+        form (:form ast)]
     (str (name form))))
 
 (defn emit-java-var
-  [ast]
-  {:pre [(= :var (:op ast))]}
-  (let [form (:form ast)]
+  [ast-opts]
+  {:pre [(= :var (:op (:ast ast-opts)))]}
+  (let [ast (:ast ast-opts)
+        form (:form ast)]
     (str (name form))))
 
 ;; entry point
 
 (defn emit-java
-  [ast]
-  ;; TODO: some multimethod ?
-  (case (:op ast)
-    :def (emit-java-def ast)
-    :const (emit-java-const ast)
-    :invoke (case (-> ast :fn :meta :name name)
-              "atom" (emit-java-atom ast)
-              "reset!" (emit-java-reset! ast))
-    :do (emit-java-do ast)
-    :let (emit-java-let ast)
-    :local (emit-java-local ast)
-    :static-call (emit-java-static-call ast)
-    :var (emit-java-var ast)
-    :else (cond
-            (:raw-forms ast) (emit-java (-> ast
-                                            :raw-forms
-                                            )))))
+  [ast-opts]
+  (let [ast (:ast ast-opts)]
+    ;; TODO: some multimethod ?
+    (case (:op ast)
+      :def (emit-java-def ast-opts)
+      :const (emit-java-const ast-opts)
+      :invoke (case (-> ast :fn :meta :name name)
+                "atom" (emit-java-atom ast-opts)
+                "reset!" (emit-java-reset! ast-opts))
+      :do (emit-java-do ast-opts)
+      :let (emit-java-let ast-opts)
+      :local (emit-java-local ast-opts)
+      :static-call (emit-java-static-call ast-opts)
+      :var (emit-java-var ast-opts)
+      :else (cond 
+              (:raw-forms ast)
+              (emit-java (assoc ast-opts :ast (-> ast :raw-forms)))))))
--- test/clj_icu_test/core_test.clj
+++ test/clj_icu_test/core_test.clj
@@ -2,7 +2,8 @@
   (:require ;;[clojure.test :refer :all]
             [clojure.tools.analyzer.jvm :as az]
             [clj-icu-test.core :refer :all]
-            [expectations :refer :all]))
+            [expectations :refer :all])
+  (:import clj_icu_test.core.AstOpts))
 ;;
 ;; C++
 ;;
@@ -12,10 +13,10 @@
 ;; bindings - def
 
 (let [ast (az/analyze '(def x 3))]
-  (expect "x = 3;" (emit-cpp ast)))
+  (expect "x = 3;" (emit-cpp (map->AstOpts {:ast ast}))))
 
 (let [ast (az/analyze '(def ^Integer x 5))]
-  (expect "int x = 5;" (emit-cpp ast)))
+  (expect "int x = 5;" (emit-cpp {:ast ast})))
 
 ;;
 ;; Java
@@ -26,63 +27,63 @@
 ;; bindings - def
 
 (let [ast (az/analyze '(def x 3))] 
-  (expect "x = 3;" (emit-java ast)))
+  (expect "x = 3;" (emit-java {:ast ast})))
 
 (let [ast (az/analyze '(def ^Integer x 5))]
-  (expect "Integer x = 5;" (emit-java ast)))
+  (expect "Integer x = 5;" (emit-java {:ast ast})))
 
 ;; language - multiple expressions
 
 ;; language - multiple expressions - do block
 
 (let [ast (az/analyze '(do (def x 3) (def y 5)))]
-  (expect (emit-java ast) ["x = 3;"
-                           "y = 5;"]))
+  (expect (emit-java {:ast ast}) ["x = 3;"
+                                  "y = 5;"]))
 
 (let [ast (az/analyze '(do (def ^Boolean x true) (def ^Long y 5)))]
-  (expect (emit-java ast) ["Boolean x = true;"
-                           "Long y = 5;"]))
+  (expect (emit-java {:ast ast}) ["Boolean x = true;"
+                                  "Long y = 5;"]))
 
 ;; bindings
 
 ;; bindings - atoms
 
 (let [ast (az/analyze '(def x (atom 11)))]
-  (expect (emit-java ast) "x = 11;"))
+  (expect (emit-java {:ast ast}) "x = 11;"))
 
 ;; bindings - reset!
 
 (let [ast (az/analyze '(do (def x (atom 11)) (reset! x 13)))]
-  (expect (emit-java ast) ["x = 11;"
-                           "x = 13;"]))
+  (expect (emit-java {:ast ast}) ["x = 11;"
+                                  "x = 13;"]))
 
 (let [ast (az/analyze '(do (def ^Long x (atom 11)) (reset! x 13)))]
-  (expect (emit-java ast) ["Long x = 11;"
-                           "x = 13;"]))
+  (expect (emit-java {:ast ast}) ["Long x = 11;"
+                                  "x = 13;"]))
 
 ;; static call (arithmetic operations)
 
 ;; static call - +
 
 (let [ast (az/analyze '(+ 11 17))]
-  (expect (emit-java ast) "11 + 17"))
+  (expect (emit-java {:ast ast}) "11 + 17"))
 
 ;; static call - /
 
 (let [ast (az/analyze '(/ 34 17))]
-  (expect (emit-java ast) "34 / 17"))
+  (expect (emit-java {:ast ast}) "34 / 17"))
 
 ;; language - multiple operands
 
 (let [ast (az/analyze '(+ 11 17 19 23))]
-  (expect (emit-java ast) "11 + 17 + 19 + 23"))
+  (expect (emit-java {:ast ast}) "11 + 17 + 19 + 23"))
 
 ;; bindings - let
 
 ;; bindings - let - 1 expression
 
 (let [ast (az/analyze '(let [x 1] (+ x 3)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   x = 1;
   x + 3;
@@ -91,7 +92,7 @@
 ;; bindings - let - 1 expression - type signature on symbol
 
 (let [ast (az/analyze '(let [^Integer x 1] (+ x 3)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   Integer x = 1;
   x + 3;
@@ -100,7 +101,7 @@
 ;; bindings - let - 2 expressions
 
 (let [ast (az/analyze '(let [x 1] (+ x 3) (+ x 5)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   x = 1;
   x + 3;
@@ -110,7 +111,7 @@
 ;; bindings - let - 2 bindings
 
 (let [ast (az/analyze '(let [x 1 y 2] (* x y)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   x = 1;
   y = 2;
@@ -120,7 +121,7 @@
 ;; bindings - let - 2 bindings - expression in binding
 
 (let [ast (az/analyze '(let [x 5 y (* x x)] (+ x y)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   x = 5;
   y = x * x;
@@ -130,7 +131,7 @@
 ;; bindings - let - nesting of let forms
 
 (let [ast (az/analyze '(let [x 5] (let [y (* x x)] (/ y x))))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   x = 5;
   {
@@ -142,7 +143,7 @@
 ;; bindings - let - atom (as bound value)
 
 (let [ast (az/analyze '(let [a (atom 23)] (+ 3 5)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   a = 23;
   3 + 5;
@@ -151,7 +152,7 @@
 ;; bindings - let - atom (as bound value) and reset!
 
 (let [ast (az/analyze '(let [a (atom 23)] (reset! a 19)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   a = 23;
   a = 19;
@@ -160,12 +161,16 @@
 ;; bindings - let - atom (as bound value) and reset! - type signature
 
 (let [ast (az/analyze '(let [^Integer a (atom 23)] (reset! a 19)))]
-  (expect (emit-java ast)
+  (expect (emit-java {:ast ast})
 "{
   Integer a = 23;
   a = 19;
 }"))
 
+;; language - nested operands
 
+(let [ast (az/analyze '(+ 3 5 (+ 1 7) 23))]
+  (expect (emit-java {:ast ast}) "3 + 5 + (1 + 7) + 23"))
 
-
+(let [ast (az/analyze '(/ 3 (/ 5 2) (/ 1 7) 23))]
+  (expect (emit-java {:ast ast}) "3 / (5 / 2) / (1 / 7) / 23"))
