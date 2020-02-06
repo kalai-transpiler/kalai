51ae206a2abba61641b9091615b135668a035247
Elango Cheran <elango.cheran@gmail.com>
2020-02-06 13:08:16 -0800

    emits let in Rust

--- test/clj_icu_test/emit/impl/rust_test.clj
+++ test/clj_icu_test/emit/impl/rust_test.clj
@@ -52,3 +52,101 @@
   (let [ast (az/analyze '(do (def ^Long x (atom 11)) (reset! x 13)))]
     (expect (emit {:ast ast :lang ::l/rust}) ["let mut x: i64 = 11;"
                                               "x = 13;"])))
+
+;; bindings - let
+
+;; bindings - let - 1 expression
+
+(defexpect bindings-let-1-expr
+  (let [ast (az/analyze '(let [x 1] (+ x 3)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let x = 1;
+  x + 3;
+}")))
+
+;; bindings - let - 1 expression - type signature on symbol
+
+(defexpect bindings-let-1-expr-type-signature
+  (let [ast (az/analyze '(let [^Integer x 1] (+ x 3)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let x: i32 = 1;
+  x + 3;
+}")))
+
+;; bindings - let - 2 expressions
+
+(defexpect bindings-let-2-expr
+  (let [ast (az/analyze '(let [x 1] (+ x 3) (+ x 5)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let x = 1;
+  x + 3;
+  x + 5;
+}")))
+
+;; bindings - let - 2 bindings
+
+(defexpect bindings-let-2-bindings
+  (let [ast (az/analyze '(let [x 1 y 2] (* x y)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let x = 1;
+  let y = 2;
+  x * y;
+}")))
+
+;; bindings - let - 2 bindings - expression in binding
+
+(defexpect bindings-let-2-bindings-with-exprs
+  (let [ast (az/analyze '(let [x 5 y (* x x)] (+ x y)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let x = 5;
+  let y = x * x;
+  x + y;
+}")))
+
+;; bindings - let - nesting of let forms
+
+(defexpect bindings-let-nested
+  (let [ast (az/analyze '(let [x 5] (let [y (* x x)] (/ y x))))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let x = 5;
+  {
+    let y = x * x;
+    y / x;
+  }
+}")))
+
+;; bindings - let - atom (as bound value)
+
+(defexpect bindings-let-atom
+  (let [ast (az/analyze '(let [a (atom 23)] (+ 3 5)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let mut a = 23;
+  3 + 5;
+}")))
+
+;; bindings - let - atom (as bound value) and reset!
+
+(defexpect bindings-let-atom-with-reset
+  (let [ast (az/analyze '(let [a (atom 23)] (reset! a 19)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let mut a = 23;
+  a = 19;
+}")))
+
+;; bindings - let - atom (as bound value) and reset! - type signature
+
+(defexpect bindings-let-atom-with-reset-type-signature
+  (let [ast (az/analyze '(let [^Integer a (atom 23)] (reset! a 19)))]
+    (expect (emit (map->AstOpts {:ast ast :lang ::l/rust}))
+"{
+  let mut a: i32 = 23;
+  a = 19;
+}")))
