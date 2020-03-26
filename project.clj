cec06eda72d971851b6dcdf647fe6775023fdbfd
Timothy Pratley <timothypratley@gmail.com>
2020-03-25 19:20:06 -0700

    [cli] Adds a command line interface for compiling

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/echeran/kalai"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :main kalai.core
   :dependencies [[expectations/clojure-test "1.2.1"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.analyzer.jvm "1.0.0"]]
-  :repl-options {:init-ns kalai.core})
+                 [org.clojure/tools.analyzer.jvm "1.0.0"]
+                 [org.clojure/tools.cli "1.0.194"]])
--- src/kalai/core.clj
+++ src/kalai/core.clj
@@ -1,2 +1,41 @@
-(ns kalai.core)
+(ns kalai.core
+  (:require [clojure.tools.cli :as cli]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [kalai.emit.langs :as l]
+            [kalai.compile :as compile]))
 
+;; TODO: probably a nicer way to define these! :)
+(def lang-names
+  (into {}
+        (for [[k _] (dissoc l/TARGET-LANGS ::l/curlybrace)]
+          [(name k) k])))
+
+(def cli-options
+  [["-i" "--in DIRECTORY" "Input directory"
+    :missing "Input directory is missing"
+    :validate [(fn check-filename [filename]
+                 (.exists (io/file filename)))
+               "File must exist"]]
+   ["-o" "--out DIRECTORY" "Output directory"
+    :default "out"]
+   ["-l" "--language LANGUAGE" (str "Target language (" (str/join ", " (keys lang-names)) ")")
+    :parse-fn lang-names
+    :validate [some?]]
+   ["-v" "--verbose" "Reports progress"]
+   ["-h" "--help" "Print usage information"]])
+
+(defn -main [& args]
+  (let [{:keys [errors summary options]} (cli/parse-opts args cli-options :strict true)]
+    (cond
+      errors (do (println (str/join \newline errors))
+                 (System/exit -1))
+      (:help options) (do (println "Options:")
+                          (println summary))
+      :else
+      (let [{:keys [verbose]} options]
+        (when verbose
+          (println "Options" (pr-str options)))
+        (compile/compile options)
+        (when verbose
+          (println "Done"))))))
