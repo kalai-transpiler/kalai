04531916ad1bbd09a44fed6d8b8c80f00063b6c1
Timothy Pratley <timothypratley@gmail.com>
2020-10-04 12:19:33 -0700

    fix demo types

--- test/kalai/demo/demo01_2.clj
+++ test/kalai/demo/demo01_2.clj
@@ -1,11 +1,11 @@
 (ns kalai.demo.demo01-2)
 
-(defn format ^String [^int num]
+(defn format ^String [^long num]
   (let [i (atom num)
         ^StringBuffer result (StringBuffer.)]
     (while (not (= @i 0))
-      (let [^int quotient (quot @i 10)
-            ^int remainder (rem @i 10)]
+      (let [^long quotient (quot @i 10)
+            ^long remainder (rem @i 10)]
         (.insert result (int 0) remainder)
         (reset! i quotient)))
     (.toString result)))
