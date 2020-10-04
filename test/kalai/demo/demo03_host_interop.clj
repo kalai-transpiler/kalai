5f7ce8b87590101bb540ee0ba752a9c44413a147
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2020-10-03 18:44:15 -0700

    no matching clause: instance-call

--- test/kalai/demo/demo03_host_interop.clj
+++ test/kalai/demo/demo03_host_interop.clj
@@ -1,9 +1,9 @@
 (ns kalai.demo.demo03-host-interop
-  (:refer-clojure :exclude [format])
   (:require [kalai.common :refer :all]))
 
 (defclass "HostInterop"
           (defn printEnvVariables ^String
             []
-            (let [^String envEditor (System/getenv "EDITOR")]
+            (let [^String envEditor (System/getenv "EDITOR")
+                  testEnd (.endsWith envEditor "X")]
               (return envEditor))))
