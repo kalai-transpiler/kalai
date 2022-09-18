34d31a00908e383a8f2fa7b08064ef960ac81e5d
Elango Cheran <elango.cheran@gmail.com>
2022-09-18 11:13:09 -0700

    fix formatting

--- examples/java/src/a/Demo03.java
+++ examples/java/src/a/Demo03.java
@@ -1,11 +1,7 @@
 package a;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+
 public class Demo03 {
-public static final void main(String[] _args) {
-System.out.println(java.lang.System.getenv("USER"));
-}
+  public static final void main(String[] _args) {
+    System.out.println(java.lang.System.getenv("USER"));
+  }
 }
